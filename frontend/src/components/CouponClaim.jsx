20fd34b7225ef8c39e426ab0eecd89ab45de5442
Atanu Hazra <imatanuhazra@gmail.com>
2025-03-13 20:29:29 +0530

    add github

--- frontend/src/components/CouponClaim.jsx
+++ frontend/src/components/CouponClaim.jsx
@@ -26,27 +26,38 @@ const CouponClaim = () => {
 
     return (
         <div className="relative flex w-full h-full items-center justify-center bg-gray-900 px-4">
-            <div className="absolute  inset-0 flex justify-center items-center">
+            <div className="absolute inset-0 flex justify-center items-center">
                 <div className="w-[400px] h-[400px] bg-blue-500 opacity-30 blur-3xl rounded-full"></div>
             </div>
 
-            <div className="relative z-10 w-full max-w-md bg-white/5 backdrop-blur-lg border border-white/20 rounded-xl shadow-xl p-6 text-center">
-                <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-white to-blue-300">
-                    Claim Your Coupon
-                </h1>
+            <div>
+                <div className="relative z-10 w-full max-w-md bg-white/5 backdrop-blur-lg border border-white/20 rounded-xl shadow-xl p-6 text-center">
+                    <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-white to-blue-300">
+                        Claim Your Coupon
+                    </h1>
 
-                {coupon ? (
-                    <p className="text-green-400 text-lg font-semibold mt-3">
-                        🎉 Your Coupon: <span className="underline">{coupon}</span>
-                    </p>
-                ) : null}
-                {message ? <p className="text-orange-400 mt-2">{message}</p> : null}
-                <button
-                    onClick={claimCoupon}
-                    className="mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg transition duration-300"
-                >
-                    Claim Coupon
-                </button>
+                    {coupon ? (
+                        <p className="text-green-400 text-lg font-semibold mt-3">
+                            🎉 Your Coupon: <span className="underline">{coupon}</span>
+                        </p>
+                    ) : null}
+                    {message ? <p className="text-orange-400 mt-2">{message}</p> : null}
+                    <button
+                        onClick={claimCoupon}
+                        className="mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg transition duration-300"
+                    >
+                        Claim Coupon
+                    </button>
+                </div>
+
+                <div className="relative flex gap-4 md:gap-5 justify-center items-center mt-10">
+                    <div>
+                        <a href="https://github.com/imatanu108/fair-coupon" target="_blank"
+                        rel="noopener noreferrer">
+                            <img src="/github.svg" alt="" width={27} />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     );
